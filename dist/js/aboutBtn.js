cfdb101204e344ccb8655fc860332a89cdc9b79d
Władysław Balandin <75247773+Double-w-B@users.noreply.github.com>
2022-04-09 00:05:35 +0200

    Images changes
    Changed .png images to .webp

--- dist/js/aboutBtn.js
+++ dist/js/aboutBtn.js
@@ -23,7 +23,7 @@ const aboutBtn = () => {
         <p>This project was created in purpose to practice web development skills. Author would be very glad if you will find it interesting or even useful.
         </p>
       
-        <p>Copyright &copy; ${new Date().getFullYear()} ODDYSEY. All rights reserved. <br/> made by <a href="https://github.com/Double-w-B" target="_blank" rel="noopener noreferrer">
+        <p>Copyright &copy; ${new Date().getFullYear()} ODYSSEY. All rights reserved. <br/> made by <a href="https://github.com/Double-w-B" target="_blank" rel="noopener noreferrer">
         Władysław Balandin
         </a>
       </p>`;
